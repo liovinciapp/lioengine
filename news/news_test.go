f1e3fd9f4263238590fa1e1d9b1df1c75c9e8ab2
Juan Alvarez <shixzie@gmail.com>
2016-11-24 15:55:36 -0400

    updated test

--- news/news_test.go
+++ news/news_test.go
@@ -11,18 +11,25 @@ func Test_NewListener(t *testing.T) {
 
 func testListen(t *testing.T) {
 	hosts := []string{
-		"http://feeds.feedburner.com/Mobilecrunch",
 		"http://feeds.feedburner.com/TechCrunch/startups",
 		"http://feeds.feedburner.com/TechCrunch/social",
+		"http://feeds.feedburner.com/Mobilecrunch",
 		"http://feeds.feedburner.com/crunchgear",
-		"http://feeds.feedburner.com/TechCrunch/Apple",
-		"http://feeds.feedburner.com/TechCrunch/Google",
-		"http://feeds.feedburner.com/TechCrunch/Twitter",
-		"http://feeds.feedburner.com/TechCrunch/Facebook",
-		"http://feeds.feedburner.com/TechCrunch/Android",
 		"http://feeds.feedburner.com/TechCrunch/Amazon",
+		"http://feeds.feedburner.com/TechCrunch/Android",
+		"http://feeds.feedburner.com/TechCrunch/Apple",
+		"http://feeds.feedburner.com/TechCrunch/Facebook",
+		"http://feeds.feedburner.com/TechCrunch/Google",
 		"http://feeds.feedburner.com/TechCrunch/Microsoft",
 		"http://feeds.feedburner.com/TechCrunch/Samsung",
+		"http://feeds.feedburner.com/TechCrunch/Twitter",
+		"https://www.wired.com/category/gear/feed/",
+		"https://www.wired.com/category/reviews/feed/",
+		"https://www.wired.com/category/design/feed/",
+		"http://www.techradar.com/rss/news/gaming",
+		"http://www.techradar.com/rss/news/portable-devices",
+		"http://www.techradar.com/rss/news/phone-and-communications",
+		"http://www.techradar.com/rss/reviews/gadgets",
 	}
 	listener, err := NewListener(hosts)
 	if err != nil {
